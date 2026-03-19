a0269e22527e0ea1163e0005a9578b18d381d056
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2026-03-19 13:48:25 +0530

    PEZY-333: Mobile | Setup | Add all dependencies to pubspec.yaml

--- mobile/pezy_mobile/.dart_tool/dartpad/web_plugin_registrant.dart
+++ mobile/pezy_mobile/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,20 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
